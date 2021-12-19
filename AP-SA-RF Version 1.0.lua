610a6e2e1a740a5c2d7ef10e17aef6c93384cb81
GhostD2R <96359879+GhostD2R@users.noreply.github.com>
2021-12-18 18:43:32 -0700

    Create AP-SA-RF Version 1.0.lua

---
+++ AP-SA-RF Version 1.0.lua
@@ -0,0 +1,89 @@
+--WZ/VG SA, AP, RF - New!
+
+--This is a Logitech Script for WZ/VG that performs the following
+--AutoPing, SteadyAim for Snipers, and RapidFire for SS and Burst Weapons
+
+
+--Config:              Change keys/mousebuttons here
+rapid_toggle=8         --Mousebutton 8
+rapidfire = false      --Do Not Change
+LC=1                   --Left Mouse Button
+RC=2                   --Right Mouse Button (Some are 2, some are 3)
+M4=4                   --Mouse Button 4 or XButton1
+APMinSpeed=10          --AutoPing Min Speed
+APMaxSpeed=70          --AutoPing Max Speed
+MinDelay=10            --Min Delay before clicking/pressing
+MaxDelay=70            --Max Delay before clicking/pressing
+AP="L"                 --My AutoPing key in the game
+SA="K"                 --My SteadyAim key in the game
+
+-- DO NOT CHANGE ANYTHING BELOW THIS LINE --
+
+function AutoPing()
+  OutputLogMessage("Running AutoPing function \n")
+  PressKey(AP)        
+  Sleep(math.random(APMinSpeed,APMaxSpeed))
+  ReleaseKey(AP)
+  OutputLogMessage("Pressed AP \n")
+end
+
+function SteadyAim()
+  OutputLogMessage("Running SteadyAim function \n")
+  PressKey(SA)
+  Sleep(3500)
+  ReleaseKey(SA)
+  OutputLogMessage("Pressed SA \n")
+end
+
+function RapidFire()    
+  OutputLogMessage("Running RapidFire function \n")
+  AutoPing()
+  repeat
+    for i = 0, 16 do
+      Sleep(100)
+      OutputLogMessage("Left Click \n")
+      PressMouseButton(LC)
+      Sleep(100)
+      OutputLogMessage("Releaase Left Click \n")
+      ReleaseMouseButton(LC)
+    end
+  until not IsMouseButtonPressed(2)  
+end
+  --until not IsMouseButtonPressed(3)
+--end
+
+EnablePrimaryMouseButtonEvents(true);          
+
+function OnEvent(event, arg)
+
+  Sleep(20)
+  
+  if (event == "MOUSE_BUTTON_PRESSED" and arg == rapid_toggle) then
+    rapidfire = not rapidfire
+    if rapidfire==true then
+      OutputLogMessage("Rapid = True \n")
+    elseif rapidfire==false then
+      OutputLogMessage("Rapidfire = False \n")
+    end
+  end
+
+  if IsMouseButtonPressed(M4) then
+    AutoPing()
+    SteadyAim()
+    Sleep(math.random(MinDelay,MaxDelay))    
+  end
+    
+  if (event == "MOUSE_BUTTON_PRESSED" and arg == 2) then                               
+    OutputLogMessage("RC engaged \n")
+    if rapidfire==true then
+      RapidFire()
+    elseif rapidfire==false then
+      repeat
+        if IsMouseButtonPressed(LC)  then
+          AutoPing()
+          Sleep(1500)
+        end
+      until not IsMouseButtonPressed(RC)
+      end    
+    end
+end
